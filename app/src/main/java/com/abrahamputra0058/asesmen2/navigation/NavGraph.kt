09b86f000ad7066fa1514419caf3ab4bd88e8ab5
Abraham Putra Berman Simbolon <abrahamputrabs@gmail.com>
2025-05-08 20:10:26 +0700

    Membuat NavGraph dan Screen

--- app/src/main/java/com/abrahamputra0058/asesmen2/navigation/NavGraph.kt
+++ app/src/main/java/com/abrahamputra0058/asesmen2/navigation/NavGraph.kt
@@ -2,9 +2,14 @@ package com.abrahamputra0058.asesmen2.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.abrahamputra0058.asesmen2.ui.screen.AboutScreen
+import com.abrahamputra0058.asesmen2.ui.screen.DetailAgendaScreen
+import com.abrahamputra0058.asesmen2.ui.screen.KEY_ID_AGENDA
 import com.abrahamputra0058.asesmen2.ui.screen.MainScreen
 
 @Composable
@@ -16,11 +21,21 @@ fun SetupNavGraph(navController: NavHostController = rememberNavController()){
         composable(route = Screen.Home.route){
             MainScreen(navController)
         }
-//        composable(route = Screen.FormAdd.route){
-//            DetailAgendaScreen(navController)
-//        }
-//        composable(route = Screen.About.route){
-//            AboutScreen(navController)
-//        }
+        composable(route = Screen.FormAdd.route){
+            DetailAgendaScreen(navController)
+        }
+        composable(route = Screen.About.route){
+            AboutScreen(navController)
+        }
+        composable(
+            route = Screen.FormEdit.route,
+            arguments = listOf(
+                navArgument(KEY_ID_AGENDA) { type = NavType.LongType }
+            )
+        ){ navBackStackEntry ->
+            val id = navBackStackEntry.arguments?.getLong(KEY_ID_AGENDA)
+            DetailAgendaScreen(navController, id)
+        }
+
     }
 }
--- app/src/main/java/com/abrahamputra0058/asesmen2/navigation/Screen.kt
+++ app/src/main/java/com/abrahamputra0058/asesmen2/navigation/Screen.kt
@@ -1,7 +1,13 @@
 package com.abrahamputra0058.asesmen2.navigation
 
+import com.abrahamputra0058.asesmen2.ui.screen.KEY_ID_AGENDA
+
 sealed class Screen(val route: String) {
     data object Home: Screen("mainScreen")
     data object FormAdd: Screen("detailScreen")
     data object About: Screen("aboutScreen")
+    data object FormEdit: Screen("detailScreen/{$KEY_ID_AGENDA}"){
+        fun withId(idAgenda: Long) = "detailScreen/$idAgenda"
+
+    }
 }
